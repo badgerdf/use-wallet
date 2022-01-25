8660544246dcd4be97c3188bfac1eb7d2cee0aaf
badgerdf <badger@gordian.agency>
2022-01-25 12:31:28 +0300

    Revert cdc arguments signature

--- src/connectors/ConnectorCDC.ts
+++ src/connectors/ConnectorCDC.ts
@@ -1,20 +1,11 @@
-import { Connector } from '../types'
+import { Connector, RpcData } from '../types'
 
 export default async function init(): Promise<Connector> {
   const { DeFiWeb3Connector } = await import('deficonnect')
 
   return {
-    web3ReactConnector({
-      chainId,
-      rpcUrl,
-    }: {
-      chainId: number
-      rpcUrl: string
-    }) {
-      return new DeFiWeb3Connector({
-        supportedChainIds: [chainId],
-        rpc: { [chainId]: rpcUrl },
-      })
+    web3ReactConnector({ chainId, rpc }: { chainId: number; rpc: RpcData }) {
+      return new DeFiWeb3Connector({ supportedChainIds: [chainId], rpc })
     },
   }
 }
