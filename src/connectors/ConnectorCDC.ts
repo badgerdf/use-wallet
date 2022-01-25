2a4c3c6a86920da2bc7bab1ce4c2ce6f9c15e5ad
badgerdf <badger@gordian.agency>
2022-01-25 13:43:54 +0300

    Add cdc arguments

--- src/connectors/ConnectorCDC.ts
+++ src/connectors/ConnectorCDC.ts
@@ -4,8 +4,23 @@ export default async function init(): Promise<Connector> {
   const { DeFiWeb3Connector } = await import('deficonnect')
 
   return {
-    web3ReactConnector({ chainId, rpc }: { chainId: number; rpc: RpcData }) {
-      return new DeFiWeb3Connector({ supportedChainIds: [chainId], rpc })
+    web3ReactConnector({
+      chainId,
+      rpc,
+      pollingInterval,
+      bridge,
+    }: {
+      chainId: number
+      rpc: RpcData
+      pollingInterval?: number
+      bridge?: string
+    }) {
+      return new DeFiWeb3Connector({
+        supportedChainIds: [chainId],
+        rpc,
+        pollingInterval,
+        bridge,
+      })
     },
   }
 }
