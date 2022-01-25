d63aee290ce2ea46671eb1c6853822f802528d45
badgerdf <badger@gordian.agency>
2022-01-25 12:00:13 +0300

    Fix cdc wallet signature

--- src/connectors/ConnectorCDC.ts
+++ src/connectors/ConnectorCDC.ts
@@ -4,8 +4,17 @@ export default async function init(): Promise<Connector> {
   const { DeFiWeb3Connector } = await import('deficonnect')
 
   return {
-    web3ReactConnector({ chainId, rpc }: { chainId: number; rpc: any }) {
-      return new DeFiWeb3Connector({ supportedChainIds: [chainId], rpc })
+    web3ReactConnector({
+      chainId,
+      rpcUrl,
+    }: {
+      chainId: number
+      rpcUrl: string
+    }) {
+      return new DeFiWeb3Connector({
+        supportedChainIds: [chainId],
+        rpc: { [chainId]: rpcUrl },
+      })
     },
   }
 }
